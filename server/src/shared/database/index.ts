35ff17ca340f6d17ce717cff162fba1c40a03c7b
Lnola <nola.luka@gmail.com>
2023-10-30 23:31:04 +0100

    :sparkles: Create a db connection

---
+++ server/src/shared/database/index.ts
@@ -0,0 +1,23 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_PORT,
+} = process.env;
+
+const pool = new Pool({
+  host: POSTGRES_HOST,
+  database: POSTGRES_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  port: Number(POSTGRES_PORT),
+});
+
+export const query = (text: string, params?: any) => pool.query(text, params);
+export default { query };
