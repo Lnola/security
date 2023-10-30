cdf229970b573b27a081ebeb9001b282c4edca1d
Lnola <nola.luka@gmail.com>
2023-10-31 00:17:30 +0100

    :art: Format file

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,7 +10,6 @@ const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use('/api', router);
-
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
