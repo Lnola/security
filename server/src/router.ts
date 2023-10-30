ea83a1f5ee1bcaaf5df38a31cda3b95085669b0f
Lnola <nola.luka@gmail.com>
2023-10-30 23:30:46 +0100

    :sparkles: Add a controller register function

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import forEach from 'lodash/forEach';
 
+type Controller = { path: string; router: Router };
+
+const controllers: Controller[] = [];
 const router = Router();
+
+forEach(controllers, (controller: Controller) => {
+  router.use(controller.path, controller.router);
+});
 
 export default router;
