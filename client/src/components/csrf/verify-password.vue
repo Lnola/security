668c2cd21180140bea809b756fd2a9bbe66375aa
Lnola <nola.luka@gmail.com>
2023-11-01 00:05:27 +0100

    :construction: Create a dummy verify password component

---
+++ client/src/components/csrf/verify-password.vue
@@ -0,0 +1,24 @@
+<template>
+  <Dialog
+    :visible="isDialogVisible"
+    @update:visible="setIsDialogVisible"
+    :style="{ width: '30rem' }"
+    modal
+    header="Verify password"
+    :draggable="false"
+  >
+  </Dialog>
+</template>
+
+<script lang="ts" setup>
+import Dialog from 'primevue/dialog';
+
+const emits = defineEmits(['update:is-dialog-visible']);
+defineProps({
+  isDialogVisible: { type: Boolean, required: true },
+});
+
+const setIsDialogVisible = (value: boolean) => {
+  emits('update:is-dialog-visible', value);
+};
+</script>
