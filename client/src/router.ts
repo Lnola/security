5ec1f6bc90068fb4a18082af8becd667e936a313
Lnola <nola.luka@gmail.com>
2023-10-31 22:38:51 +0100

    :sparkles: Add a csrf page template

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import SqlInjection from '@/pages/sql-injection-page.vue';
+import Csrf from '@/pages/csrf-page.vue';
 
 const routes = [
   {
     path: '/sql-injection',
     name: 'SqlInjection',
     component: SqlInjection,
+  },
+  {
+    path: '/csrf',
+    name: 'Csrf',
+    component: Csrf,
   },
   {
     path: '/:catchAll(.*)',
