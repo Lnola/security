a2bc0497d06442ff391d395a272a36323b1041ba
Lnola <nola.luka@gmail.com>
2023-10-31 19:47:53 +0100

    :sparkles: Add the sql injection page to the router

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/pages/home-page.vue';
+import SqlInjection from '@/pages/sql-injection-page.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/sql-injection',
+    name: 'SqlInjection',
+    component: SqlInjection,
   },
   {
     path: '/:catchAll(.*)',
