b8d0418751b748de08a3d9c44ec0d958007ee5b7
Lnola <nola.luka@gmail.com>
2023-10-31 21:00:17 +0100

    :sparkles: Add a common sy-apply component

---
+++ client/src/components/sy-apply.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="flex align-items-center p-card-subtitle my-0">
+    <pre
+      class="ml-2 mr-1 my-0 text-overflow-ellipsis overflow-hidden surface-100 border-round"
+      >{{ text }}</pre
+    >
+    <Button
+      class="h-auto"
+      @click="emits('click', text)"
+      icon="pi pi-check-square"
+      severity="secondary"
+      rounded
+      text
+    />
+  </span>
+</template>
+
+<script lang="ts" setup>
+import Button from 'primevue/button';
+
+const emits = defineEmits(['click']);
+defineProps({
+  text: { type: String, required: true },
+});
+</script>
