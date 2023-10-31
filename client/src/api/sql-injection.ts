567a6203d819c0f3a66f2f253140e7e247e1a6d3
Lnola <nola.luka@gmail.com>
2023-10-31 21:26:15 +0100

    :sparkles: Add correct usage for sql injection api methods

--- client/src/api/sql-injection.ts
+++ client/src/api/sql-injection.ts
@@ -1,6 +1,22 @@
-export const searchVulnarable = (username: string): Promise<string> => {
-  return Promise.resolve(username + ' vulnarable');
+import request from './request';
+import { extractData } from './helpers';
+
+const urls = {
+  root: '/sql-injection',
+  get vulnarable() {
+    return this.root + '/vulnarable';
+  },
+  get secure() {
+    return this.root + '/secure';
+  },
 };
+
+export const searchVulnarable = (username: string): Promise<string> => {
+  const params = { username };
+  return request.get(urls.vulnarable, { params }).then(extractData);
+};
+
 export const searchSecure = (username: string): Promise<string> => {
-  return Promise.resolve(username + ' secure');
+  const params = { username };
+  return request.get(urls.secure, { params }).then(extractData);
 };
