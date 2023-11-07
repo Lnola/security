23d487e3314c1b93ee25440d5cae225747b5900d
Lnola <nola.luka@gmail.com>
2023-11-07 18:24:41 +0100

    :construction: Create dummy methods for csrf controller

--- server/src/csrf/controller.ts
+++ server/src/csrf/controller.ts
@@ -2,6 +2,30 @@ import { Request, Response, NextFunction } from 'express';
 import { OK } from 'http-status';
 import initializeAdmin from 'shared/database/initialization/admin';
 
+export const updatePasswordVulnarable = async (
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  return res.json(OK);
+};
+
+export const updatePasswordSecure = async (
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  return res.json(OK);
+};
+
+export const verifyPassword = async (
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  return res.json(OK);
+};
+
 export const resetAdminTable = async (
   _req: Request,
   res: Response,
