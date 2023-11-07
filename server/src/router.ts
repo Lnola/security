fc382d5809917351689380267ca7f0b55ede98f9
Lnola <nola.luka@gmail.com>
2023-11-07 18:25:36 +0100

    :sparkles: Register the csrf controller

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import forEach from 'lodash/forEach';
 import sqlInjection from './sql-injection';
+import csrf from './csrf';
 
 type Controller = { path: string; router: Router };
 
-const controllers: Controller[] = [sqlInjection];
+const controllers: Controller[] = [sqlInjection, csrf];
 const router = Router();
 
 forEach(controllers, (controller: Controller) => {
