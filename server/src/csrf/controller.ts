7efdbc8965b6308be66f991e1c9595ec3682379a
Lnola <nola.luka@gmail.com>
2023-11-07 19:04:01 +0100

    :sparkles: Implement verify password controller method

--- server/src/csrf/controller.ts
+++ server/src/csrf/controller.ts
@@ -1,6 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
-import { OK } from 'http-status';
-import initializeAdmin from 'shared/database/initialization/admin';
+import { FORBIDDEN, OK } from 'http-status';
+import db from '../shared/database';
+import initializeAdmin from '../shared/database/initialization/admin';
+import HttpError from 'shared/error/http-error';
+
+type VerifyPasswordDto = {
+  username: string;
+  password: string;
+};
+
+const queries = {
+  getUserByDto: ({ username, password }: VerifyPasswordDto) => {
+    return db.query(
+      'SELECT username, password FROM admin WHERE username = $1 AND password = $2;',
+      [username, password]
+    );
+  },
+};
 
 export const updatePasswordVulnarable = async (
   _req: Request,
@@ -19,10 +35,15 @@ export const updatePasswordSecure = async (
 };
 
 export const verifyPassword = async (
-  _req: Request,
+  req: Request,
   res: Response,
-  _next: NextFunction
+  next: NextFunction
 ) => {
+  const dto = req.query as VerifyPasswordDto;
+  const user = (await queries.getUserByDto(dto)).rows[0];
+  if (!user) {
+    return next(new HttpError(FORBIDDEN, 'Invalid username or password!'));
+  }
   return res.json(OK);
 };
 
