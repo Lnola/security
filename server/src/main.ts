0fd3ec2583df06b107444bbb87f90ee68224d777
Lnola <nola.luka@gmail.com>
2023-11-12 23:42:43 +0100

    :rocket: Initialize on start if env is production

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './router';
 import errorHandler from './shared/error/error-handler';
+import initializeAdmin from 'shared/database/initialization/admin';
+import initializeUsers from 'shared/database/initialization/users';
 
 dotenv.config();
 
@@ -13,5 +15,12 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', router);
 app.use(errorHandler);
+
+(async () => {
+  if (process.env.NODE_ENV === 'production') {
+    await initializeAdmin();
+    await initializeUsers();
+  }
+})();
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
