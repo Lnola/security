5bbeb4a019f8e6121a5e675c81fb1ffedf3a9d9c
Lnola <nola.luka@gmail.com>
2023-11-12 14:07:58 +0100

    :sparkles: Get is secure from query

--- client/src/pages/reroute-page.vue
+++ client/src/pages/reroute-page.vue
@@ -1,3 +1,9 @@
-<template>Reroute page</template>
+<template>Is Secure: {{ isSecure }}</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+import yn from 'yn';
+
+const { query } = useRoute();
+const isSecure = yn(query.isSecure);
+</script>
