b97a277ce350709007676b95402a15e42236f372
Lnola <nola.luka@gmail.com>
2023-11-12 21:44:24 +0100

    :sparkles: Add the reroute page content

--- client/src/pages/reroute-page.vue
+++ client/src/pages/reroute-page.vue
@@ -1,11 +1,45 @@
-<template>Is Secure: {{ isSecure }}</template>
+<template>
+  <Card class="app-content">
+    <template #title>
+      CSRF attack (
+      <pre class="surface-100 inline">isSecure: {{ isSecure }}</pre>
+      )
+    </template>
+    <template #content>
+      <h3>The attack has been initiated</h3>
+      <p class="max-w-30rem">
+        This page sends the update password api request which will or will not
+        trigger a password change depending on whether the isSecure option is
+        set.
+        <br />
+        <br />
+        If the safety is <b>FALSE</b>, the attack should succed and you should see a
+        toast stating: <pre>{{ SUCCESS_MESSAGE }}</pre>
+        <br />
+        If the safety is <b>TRUE</b>, the attack should fail and you should see a toast
+        stating: <pre>{{ FAILURE_MESSAGE }}</pre>
+      </p>
+
+      <RouterLink :to="{ name: 'Csrf' }">
+        <Button >Go back to CSRF page</Button>
+      </RouterLink>
+    </template>
+  </Card>
+</template>
 
 <script lang="ts" setup>
 import yn from 'yn';
 import { onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import { useToast } from 'primevue/usetoast';
+import Card from 'primevue/card';
+import Button from 'primevue/button';
 import { updatePasswordVulnarable, updatePasswordSecure } from '@/api/csrf';
+
+const USERNAME = 'admin';
+const NEW_PASSWORD = '123';
+const SUCCESS_MESSAGE = `Password updated to "${NEW_PASSWORD}"!`;
+const FAILURE_MESSAGE = 'Update password blocked!';
 
 const toast = useToast();
 const { query } = useRoute();
@@ -16,17 +50,19 @@ onMounted(async () => {
 });
 
 const changePassword = async () => {
-  const username = 'admin';
-  const newPassword = '123';
   try {
     const updatePasswordMethod = isSecure
       ? updatePasswordSecure
       : updatePasswordVulnarable;
-    await updatePasswordMethod({ username, newPassword });
+    await updatePasswordMethod({
+      username: USERNAME,
+      newPassword: NEW_PASSWORD,
+      token: null,
+    });
     toast.add({
       severity: 'success',
       summary: 'Success',
-      detail: 'Password updated to "1234"!',
+      detail: SUCCESS_MESSAGE,
       life: 3000,
     });
   } catch (error) {
@@ -34,7 +70,7 @@ const changePassword = async () => {
     toast.add({
       severity: 'error',
       summary: 'Error',
-      detail: 'Update password blocked!',
+      detail: FAILURE_MESSAGE,
       life: 3000,
     });
   }
